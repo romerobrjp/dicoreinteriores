66963848926c03d7fe5c185f5767506a7a925e26
Manoel Amaro <mano.lira@gmail.com>
2015-11-24 20:29:44 -0300

    Finished initial integration with the template

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,8 +5,10 @@ Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'images')
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w( admin/application.js admin/application.css )
 Rails.application.config.assets.precompile << Proc.new { |path| path =~ /(bootstrap|font-awesome)/ and File.extname(path).in?(['.otf', '.eot', '.svg', '.ttf', '.woff', '.woff2']) }
+Rails.application.config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
