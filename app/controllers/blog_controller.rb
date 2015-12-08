ecb270b541ce1a745cbb1cc4d194075d13569dd7
Romero Meireles <romero.mfm@gmail.com>
2015-12-08 19:09:12 -0300

    Implementing blog righ-section

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -2,6 +2,9 @@ class BlogController < ApplicationController
   layout "blog"
 
   def index
+    @archives = Post.order(created_at: :desc).limit(5).each
+    # @archives = PostQuery.new.search.archives
+    @categories = Category.all
     @posts = Post.published_ordered.page(params.fetch(:page, 1))
   end
 
