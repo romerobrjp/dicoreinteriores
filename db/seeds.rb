f0ee796a4dcc6e229c829fce2b17b38be16fd90a
Manoel Amaro <mano.lira@gmail.com>
2016-01-08 11:09:01 -0300

    adding seeds control

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,43 +6,44 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create!(
-  name: 'Admin',
-  surname: 'Admin',
-  email: 'admin@admin.com',
-  cpf: '07644271481',
-  phone1: '996-447-337',
-  password: '1234Mudar',
-  type: 'Admin'
-)
+if (!User.find_by_email('admin@admin.com'))
+  puts 'creating admin'
+  User.create!(
+    name: 'Admin',
+    surname: 'Admin',
+    email: 'admin@admin.com',
+    cpf: '07644271481',
+    phone1: '996-447-337',
+    password: '1234Mudar',
+    type: 'Admin'
+  )
+end
 
-User.create!(
-  name: 'Admin',
-  surname: 'Teste',
-  email: 'romero.mfm@gmail.com',
-  cpf: '07644271481',
-  phone1: '996-447-337',
-  # password: BCrypt::Password.create('123123'),
-  password: '123123123',
-  type: 'Admin'
-)
+if (!Category.find_by_name('Inspiração'))
+  puts 'creating category Inspiração'
+  Category.create!(
+    name: "Inspiração",
+    show_on_header: true
+  )
+end
 
-Category.create!(
-  name: "Inspiração",
-  show_on_header: true
-)
+if (!Category.find_by_name('Faça você mesmo'))
+  puts 'creating category Faça você mesmo'
+  Category.create!(
+    name: "Faça você mesmo",
+    show_on_header: true
+  )
+end
 
-Category.create!(
-  name: "Faça você mesmo",
-  show_on_header: true
-)
-
-Preference.create!(
-  specific_id: 'global_prefs',
-  color_1: "#adc1da",
-  color_2: "#bfcfe2",
-  color_3: "#cedae9",
-  color_4: "#d5dfec",
-  color_5: "#d6e0ed",
-  user_id: nil,
-)
+if (!Preference.find_by_specific_id('global_prefs'))
+  puts 'creating global_prefs'
+  Preference.create!(
+    specific_id: 'global_prefs',
+    color_1: "#adc1da",
+    color_2: "#bfcfe2",
+    color_3: "#cedae9",
+    color_4: "#d5dfec",
+    color_5: "#d6e0ed",
+    user_id: nil
+  )
+end
